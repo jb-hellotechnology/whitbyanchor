d64ba5a76f9bb2c72db6211159cfb65491691a9b
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2026-04-20 14:55:51 +0100

    Update content-home.php

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -16,18 +16,8 @@
 
 	<?php whitbyanchor_post_thumbnail(); ?>
 
-	<div class="entry-content flow">
+	<div class="entry-content flow week-in-pictures npg-gallery-init">
 		<?php
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/icons/apple-icon-180x180.png" alt="Whitby Anchor" />
-				<?php
-				whitbyanchor_posted_on();
-				whitbyanchor_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif;
 		the_content(
 			sprintf(
 				wp_kses(
@@ -40,13 +30,6 @@
 					)
 				),
 				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'whitbyanchor' ),
-				'after'  => '</div>',
 			)
 		);
 		?>
