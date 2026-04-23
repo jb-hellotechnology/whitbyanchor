3a8cef4e32a887425d374b0d57843ff69d5a084d
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2026-04-23 11:53:27 +0100

    subscribe

--- footer.php
+++ footer.php
@@ -59,7 +59,7 @@
 	<button class="close">Close</button>
 	<div>
 	<iframe width="360" height="540" src="https://55fe7bef.sibforms.com/serve/MUIFAPpLZHCi43qNpINCiqS4InXyrFol5HEKl_Z2TN6HiYowk91-mztfi8dkMZgFLCFn1lUusVf1gTX-1CAsD6g05Em3IkJ9IDtw7wweEHb9Ye0DxYTxZTiJpUa0yN5c0LnZcyxxI5uCBYwcXXkksoejughDncvZDLuOr8xec5FMdLJExjg8KcYBGUFHe_EHK2uo9809gcaH4uRQyA==" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>
-	<!-- <a class="button secondary" href="/subscribe">Support Local Journalism</a> -->
+	<a class="button secondary" href="/subscribe">Support Local Journalism</a>
 	</div>
 </div>
 
